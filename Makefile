f29931e4d0785cce00ab75e2096370e9d53a6932
Andrew Kennedy <akennedy2292@gmail.com>
2018-01-18 22:11:50 +0000

    add web server

--- Makefile
+++ Makefile
@@ -9,15 +9,15 @@ M = $(shell printf "\033[34;1m▶\033[0m")
 all: clean genrpc run
 
 .PHONY: clean
-clean: ; $(info $(M) cleaning…)	@
+clean: ; $(info $(M) cleaning)	@
 	@rm -rf rpc/out
 
 .PHONY: genrpc
-genrpc: ; $(info $(M) generating proto buf files…)	@
+genrpc: ; $(info $(M) generating proto files)	@
 	$Q @mkdir ./rpc/out
 	$Q @protoc -I ./rpc --twirp_out=./rpc/out --go_out=./rpc/out ./rpc/home.proto
 
 .PHONY: run
-run: ; $(info $(M) runnin service…)	@
+run: ; $(info $(M) running service)	@
 	$Q $(GO) run main.go
 
--- main.go
+++ main.go
@@ -2,20 +2,72 @@ package main
 
 import (
   "context"
+  "log"
   "net/http"
+  "os"
+  "os/signal"
+  "time"
+  "syscall"
 
   pb "cryptic.town/rpc/out"
 )
 
-type Server struct {}
+type Server struct {
+  logger *log.Logger
+  mux    *http.ServeMux
+}
 
-func (s *Server) MakeHome(ctx context.Context, req *pb.User) (*pb.Home, error) {
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.mux.ServeHTTP(w, r)
+}
+
+type RPCServer struct {}
+
+func (s *RPCServer) MakeHome(ctx context.Context, req *pb.User) (*pb.Home, error) {
   return &pb.Home{Name: req.Name, Body: "Hi "+req.Name}, nil
 }
 
+type WebServer struct {}
+
+func (s *WebServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("hi!"))
+}
+
 func main() {
-  twirpHandler := pb.NewCrypticTownServer(&Server{}, nil)
+  stop := make(chan os.Signal, 2)
+  signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+  logger := log.New(os.Stdout, "", 0)
+
+  // RPC Methods
+  twirpHandler := pb.NewCrypticTownServer(&RPCServer{}, nil)
   mux := http.NewServeMux()
   mux.Handle(pb.CrypticTownPathPrefix, twirpHandler)
-  http.ListenAndServe(":8081", mux)
+
+  // Serve HTML
+  mux.Handle("/", &WebServer{})
+
+  // Setup server
+  port := ":" + os.Getenv("PORT")
+  if port == ":" { port = ":8081" }
+
+  s := &Server{ logger: log.New(os.Stdout, "", 0), mux: mux, }
+  h := &http.Server{Addr: port, Handler: s}
+
+  go func() {
+    logger.Printf("Listening on %s\n", port)
+
+    if err := h.ListenAndServe(); err != nil {
+      logger.Fatal(err)
+    }
+  }()
+
+  <-stop
+
+  logger.Println("\nShutting down the server...")
+
+  ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+  h.Shutdown(ctx)
+
+  logger.Println("Server gracefully stopped")
 }
